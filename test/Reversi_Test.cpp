7806663cf9503f2d9798cfd20d71349394621791
CostaCesar <caioplays@outlook.com>
2025-01-07 16:37:19 -0300

    ADD: Teste Minimo Do Reversi

--- test/Reversi_Test.cpp
+++ test/Reversi_Test.cpp
@@ -2,22 +2,27 @@
 
 #include "Game_Reversi.hpp"
 
+// "./bin/Reversi_Test < ./test/reversi_input.txt"
 TEST_CASE("Reversi Creation")
 {
     Board* object = new Game_Reversi();
+    REQUIRE(object != nullptr);
 
-    switch (object->Play())
-    {
-    case 1:
-        std::cout << "# PLAYER 1 WINS #" << std::endl;
-        break;
-    case 2:
-        std::cout << "# PLAYER 2 WINS #" << std::endl;
-        break;
-    case 0:
-        std::cout << "# DRAW #" << std::endl;
-        break;
-    }
+    uint winner = object->Play();
+    CHECK(winner == 2);
+
+    // switch (object->Play())
+    // {
+    // case 1:
+    //     std::cout << "# PLAYER 1 WINS #" << std::endl;
+    //     break;
+    // case 2:
+    //     std::cout << "# PLAYER 2 WINS #" << std::endl;
+    //     break;
+    // case 0:
+    //     std::cout << "# DRAW #" << std::endl;
+    //     break;
+    // }
     
     delete object;
 }
