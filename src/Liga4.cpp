49c9fa7d11c1387bb72bbc8d1f8790e910e40d27
Leonardo Barreto <leonardobarretogaiao@gmail.com>
2025-01-25 14:14:18 -0300

    Versão quase final do Liga4 base. Melhorias: -ReadMove funcional -usa símbolos ao invés de 1 e 2
    Problemas:
    -O IsDraw não parece correto, estou questionando se ele é realmente necessário

--- src/Liga4.cpp
+++ src/Liga4.cpp
@@ -9,39 +9,47 @@ Liga4::~Liga4() {}
 
 uint Liga4::Play() {
     Vec2 last_move;
-
     uint num_plays = GetSize().x * GetSize().y;
 
-    while (num_plays > 0)
-    {
+    while (num_plays > 0) {
+        #if defined(_WIN32) || defined(_WIN64)
+            system("cls");
+        #else
+            system("clear");
+        #endif
+
+        std::cout << "Player " << current_player << '\n' << std::endl;
         Draw();
 
-        while (1)
-        {
-            try
-            {
+        bool valid_move = false;
+        while (!valid_move) {
+            try {
                 last_move = ReadMove();
+                valid_move = true;
+            } catch (const std::exception& e) {
+                std::cout << "ERRO: " << e.what() << std::endl;
             }
-            catch(const std::exception& e)
-            {
-                std::cout << "Entrada invalida!" << std::endl;
-                continue;
-            }
-            break;
         }
-        
-        Piece* piece = new Piece(last_move, current_player);
+
+        char symbol = (current_player == 1) ? 'X' : 'O';
+        Piece* piece = new Piece(last_move, current_player, symbol);
 
         if (!AddPiece(piece)) {
-            std::cout << "ERRO: jogada inválida" << std::endl;
             delete piece;
             continue;
         }
 
-        if (CheckWin(current_player, last_move))
+        if (CheckWin(current_player, last_move)) {
+            std::cout << "\n\nPlayer " << current_player << " venceu!" << std::endl;
             break;
+        }
 
         num_plays--;
+        if (IsDraw()) {
+            std::cout << "\n\nEmpate" << std::endl;
+            break;
+        }
+
         NextPlayer();
     }
 
@@ -50,48 +58,48 @@ uint Liga4::Play() {
 }
 
 uint Liga4::EmptyRow(uint column) {
-    for (uint row = GetSize().y ; row > 0; --row) {
+    for (uint row = GetSize().y; row > 0; --row) {
         if (GetPiece(Vec2{column, row-1}) == nullptr) {
             return row-1;
         }
     }
-
     return GetSize().y;
 }
 
-Vec2 Liga4::ReadMove()
-{
+Vec2 Liga4::ReadMove() {
     Vec2 output;
+    std::string input_str;
+    std::getline(std::cin, input_str);
 
-    try
-    {
-        output.x = GetUintFromInput();
+    for (size_t i = 0; i < input_str.length(); i++) {
+        if (!isdigit(input_str[i])) { // isdigit é o caminho
+            throw std::invalid_argument("Entrada invalida!");
+        }
     }
-    catch(const std::exception& e)
-    {
-        // Flush Bad Stream
-        FlushInput();
-        throw e;
+
+    output.x = std::atoi(input_str.c_str());
+    if (output.x < 0 || output.x >= GetSize().x) {
+        throw std::out_of_range("Jogada invalida!");
     }
 
     output.y = EmptyRow(output.x);
+    if (output.y == GetSize().y) {
+        throw std::out_of_range("Jogada invalida!");
+    }
+
     return output;
 }
 
-uint Liga4::GetWinner()
-{
-    if(IsDraw())
-        return 0;
-    else return current_player;
+uint Liga4::GetWinner() {
+    return IsDraw() ? 0 : current_player;
 }
 
 bool Liga4::CheckWin(uint player, const Vec2& last_move) {
-    for (uint i = 0; i < 8; i++)
-    {
-        if(MatchUntilStep(last_move, (Direction) i, 3) == MatchReturn::Matched)
+    for (uint i = 0; i < 8; i++) {
+        if (MatchUntilStep(last_move, (Direction)i, 3) == MatchReturn::Matched) {
             return true;
+        }
     }
-
     return false;
 }
 
