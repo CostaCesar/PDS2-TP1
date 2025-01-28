245baa35bb68d8bf30013673001c7cf41ece60dd
vini-alg <vinicius.alcantara115935@gmail.com>
2025-01-28 07:10:17 -0300

    FIX: documentação jogo da velha infinito

--- inc/Game_Velha_Infinity.hpp
+++ inc/Game_Velha_Infinity.hpp
@@ -3,7 +3,7 @@
 #include "Game_Velha.hpp"
 
 /**
- * @class Game_Liga4
+ * @class Game_Velha_Infinity
  * @brief Classe que representa o jogo "Jogo da velha infinito", que herda de `Game_Velha`.
  */
 class Game_Velha_Infinity : public Game_Velha
@@ -15,13 +15,14 @@ public:
      * 
      * Imprime um tabuleiro com as posições respectivas para cada jogada e admite uma entrada que representa a jogada de cada jogador alternando entre dois jogadores.
      * Quando o jogo acaba, seja pela vitoria de um jogador ou por empate, ele imprime a variavel que representa o resultado do jogo.
+     * A diferença do jogo classico é que cada jogador pode ter até no maximo 3 peças no tabuleiro por vez, ou seja a cada 3 jogadas, a primeira peça é deletada.
      * 
      * @return uint 1 ou 2 para a vitoria do jogador respectivo e 0 para empate.
      */
     uint Play() override;
 
     /**
-     * @brief Construtor da classe `Game_Velha`.
+     * @brief Construtor da classe `Game_Velha_Infinity`.
      * 
      */
     Game_Velha_Infinity();
